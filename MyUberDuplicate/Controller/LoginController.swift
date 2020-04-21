293e2aa8cfb70f1b99f7576504ffcbf867d86f78
Eric Grant <senatorericgrant@gmail.com>
2020-04-22 00:03:24 +0700

    refactor: setup rootView Controller

---
+++ MyUberDuplicate/Controller/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  MyUberDuplicate
+//
+//  Created by Eric Grant on 21.04.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
