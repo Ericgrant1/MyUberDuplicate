248ea891d56cc4bab9edf843010016094850691d
Eric Grant <senatorericgrant@gmail.com>
2020-04-22 00:52:19 +0700

    feat: add start UI

--- MyUberDuplicate/Controller/LoginController.swift
+++ MyUberDuplicate/Controller/LoginController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    // MARK: - Properties
+    
+    private let tittleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        view.addSubview(tittleLabel)
+        tittleLabel.translatesAutoresizingMaskIntoConstraints = false
+        tittleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        tittleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                         constant: 0).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
