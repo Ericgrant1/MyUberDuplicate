283bc1d8c0e9fec1ec6a043c2010c12d328ac249
Eric Grant <senatorericgrant@gmail.com>
2020-04-25 15:56:05 +0700

    feat: add Location input View UI & Dismissal

--- MyUberDuplicate/Controller/HomeController.swift
+++ MyUberDuplicate/Controller/HomeController.swift
@@ -18,6 +18,7 @@ class HomeController: UIViewController {
     private let locationManager = CLLocationManager()
     
     private let inputActivationView = LocationInputActivationView()
+    private let locationInputView = LocationInputView()
        
     // MARK: - Lifecycle
     
@@ -76,6 +77,23 @@ class HomeController: UIViewController {
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
     }
+    
+    func configureLocationInputView() {
+        locationInputView.delegate = self
+        
+        view.addSubview(locationInputView)
+        locationInputView.anchor(top: view.topAnchor,
+                                 left: view.leftAnchor,
+                                 right: view.rightAnchor,
+                                 height: 200)
+        locationInputView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.locationInputView.alpha = 1
+        }) { _ in
+            print("DEBUG: Present table view..")
+        }
+    }
 }
 
 // MARK: - LocationServices
@@ -110,8 +128,25 @@ extension HomeController: CLLocationManagerDelegate {
     }
 }
 
+// MARK: - LocationInputActivationViewDelegate
+
 extension HomeController: LocationInputActivationViewDelegate {
     func presentLocationInputView() {
-        print("DEBUG: Handle present location input view..")
+        inputActivationView.alpha = 0
+        configureLocationInputView()
+    }
+}
+
+// MARK: - LocationInputViewDelegate
+
+extension HomeController: LocationInputViewDelegate {
+    func dismissLocationInputView() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.locationInputView.alpha = 0
+        }) { _ in
+            UIView.animate(withDuration: 0.3) {
+                self.inputActivationView.alpha = 1
+            }
+        }
     }
 }
---
+++ MyUberDuplicate/View/LocationInputView.swift
@@ -0,0 +1,55 @@
+//
+//  LocationInputView.swift
+//  MyUberDuplicate
+//
+//  Created by Eric Grant on 25.04.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import UIKit
+
+protocol LocationInputViewDelegate: class {
+    func dismissLocationInputView()
+}
+
+class LocationInputView: UIView {
+
+    // MARK: - Properties
+    
+    weak var delegate: LocationInputViewDelegate?
+    
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "baseline_arrow_back_black_36dp-1").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleBackTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addShadow()
+        
+        backgroundColor = .white
+        
+        addSubview(backButton)
+        backButton.anchor(top: topAnchor,
+                          left: leftAnchor,
+                          paddingTop: 44,
+                          paddingLeft: 12,
+                          width: 24,
+                          height: 24)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleBackTapped() {
+        delegate?.dismissLocationInputView()
+    }
+}
