2a2ff5c19c70d86e3ed0d39eabe856b9c84f2ccd
Eric Grant <senatorericgrant@gmail.com>
2020-04-25 22:27:05 +0700

    feat: add Setup Locations tableView

---
+++ MyUberDuplicate/View/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  MyUberDuplicate
+//
+//  Created by Eric Grant on 25.04.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
