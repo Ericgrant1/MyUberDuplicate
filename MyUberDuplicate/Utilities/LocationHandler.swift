d00001f832665b09786ac6d2577df975b433b77a
Eric Grant <senatorericgrant@gmail.com>
2020-04-27 21:46:04 +0700

    feat: implement Register Driver & upload location to Database

---
+++ MyUberDuplicate/Utilities/LocationHandler.swift
@@ -0,0 +1,30 @@
+//
+//  LocationHandler.swift
+//  MyUberDuplicate
+//
+//  Created by Eric Grant on 27.04.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationHandler()
+    var locationManager: CLLocationManager!
+    var location: CLLocation?
+    
+    override init() {
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+}
