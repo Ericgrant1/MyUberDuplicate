a789cd6ad188818262b13c71f2925aa4c3d7cdd3
Eric Grant <senatorericgrant@gmail.com>
2020-04-28 00:14:18 +0700

    feat: render custom Driver Annotation

--- MyUberDuplicate/Controller/HomeController.swift
+++ MyUberDuplicate/Controller/HomeController.swift
@@ -11,6 +11,7 @@ import Firebase
 import MapKit
 
 private let reuseIdentifier = "LocationCell"
+private let annotationIdentifier = "DriverAnnotation"
 
 class HomeController: UIViewController {
     
@@ -110,6 +111,7 @@ class HomeController: UIViewController {
         
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        mapView.delegate = self
     }
     
     func configureLocationInputView() {
@@ -147,6 +149,20 @@ class HomeController: UIViewController {
                                  height: height)
         
         view.addSubview(tableView)
+    }
+}
+
+// MARK: - MKMapViewDelegate
+
+extension HomeController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? DriverAnnotation {
+            let view = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            view.image = #imageLiteral(resourceName: "chevron-sign-to-right")
+            return view
+        }
+        return nil
     }
 }
 
