54f59f7ee5e341ef56f52d8fa840259db13d2a4a
Eric Grant <senatorericgrant@gmail.com>
2020-05-05 17:04:34 +0700

    feat: implement Ride Action Config

--- MyUberDuplicate/View/RideActionView.swift
+++ MyUberDuplicate/View/RideActionView.swift
@@ -13,6 +13,40 @@ protocol RideActionViewDelegate: class {
     func uploadTrip(_ view: RideActionView)
 }
 
+enum RideActionViewConfiguration {
+    case requestRide
+    case tripAccepted
+    case pickupPassenger
+    case tripInProgress
+    case endTrip
+    
+    init() {
+        self = .requestRide
+    }
+}
+
+enum ButtonAction: CustomStringConvertible {
+    case requestRide
+    case cancel
+    case getDirections
+    case pickup
+    case dropOff
+    
+    var description: String {
+        switch self {
+        case .requestRide: return "CONFIRM UDERX"
+        case .cancel: return "CANCEL RIDE"
+        case .getDirections: return "GET DIRECTIONS"
+        case .pickup: return "PICKUP PASSENGER"
+        case .dropOff: return "DROP OFF PASSENGER"
+        }
+    }
+    
+    init() {
+        self = .requestRide
+    }
+}
+
 class RideActionView: UIView {
 
     // MARK: - Properties
@@ -23,6 +57,9 @@ class RideActionView: UIView {
             addressLabel.text = destination?.address
         }
     }
+    
+    var config = RideActionViewConfiguration()
+    var buttonAction = ButtonAction()
     
     weak var delegate: RideActionViewDelegate?
     
@@ -131,5 +168,11 @@ class RideActionView: UIView {
     
     @objc func actionButtonPressed() {
         delegate?.uploadTrip(self)
+    }
+    
+    // MARK: Helper Functions
+    
+    func configureUI(withConfig config: RideActionViewConfiguration) {
+        
     }
 }
