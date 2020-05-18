90b44cb75b7034b3f73fd5f70b43df5bbccc21f4
Eric Grant <senatorericgrant@gmail.com>
2020-05-18 23:09:50 +0700

    feat: get started with the Start Menu

---
+++ MyUberDuplicate/Controller/SideMenu/MenuController.swift
@@ -0,0 +1,28 @@
+//
+//  MenuController.swift
+//  MyUberDuplicate
+//
+//  Created by Eric Grant on 18.05.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helper Functions
+
+}
